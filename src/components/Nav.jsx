eb5a1177027480020936f9d154cdc86ab5e504b8
chayan-raheja <rahejachayan@gmail.com>
2026-01-04 21:15:48 +0530

    fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,15 +5,15 @@ const Nav = () => {
   const [pkname, setpkname] = useState([]);
 
   return (
-    <div className="w-full py-6 bg-green-200/75 flex items-center gap-3 justify-between ">
-      <div className="relative  sm:px-2 h-full flex items-center  ">
+    <div className="w-full py-6 px-2 bg-green-200/75 flex items-center gap-3 justify-between ">
+      <div className=" relative sm:px-2 h-full flex items-center  ">
         <img
-          className="absolute invisible cursor-pointer md:visible w-32 max-w-none"
+          className="hidden cursor-pointer md:block w-32 max-w-none"
           src="/poka.webp"
           alt="pokemon"
         />
         <img
-          className=" absolute visible md:invisible w-12 max-w-none"
+          className="  visible md:invisible w-12 max-w-none"
           src="/ball.png"
           alt="pokemon"
         />{" "}
@@ -25,33 +25,33 @@ const Nav = () => {
           onChange={(e) => {
             setpkname(e.target.value);
           }}
-          className=" md:visible border rounded-2xl sm:w-xs sm:px-4 sm:py-2 md:px-5 md:py-3.5 md:w-xl"
+          className="  border rounded-2xl w-52 px-1 py-2 md:px-5 md:py-3.5 md:w-xl"
           type="text"
           placeholder="Search Pokemon by Name"
         />
       </div>
       <div className=" relative">
-       <div className="absolute flex gap-7 items-center justify-center   ">
-         <Link
-          to="/Home"
-          className=" invisible md:visible text-xl font-semibold text-black/80"
-        >
-          Home
-        </Link>
-        <Link
-          to="/Card"
-          className=" invisible md:visible text-xl font-semibold text-black/80"
-        >
-          Card
-        </Link>
-        <Link
-          to="/Contact"
-          className=" invisible md:visible text-xl font-semibold text-black/80"
-        >
-          Contact Us
-        </Link>
-       </div>
-        <i className="absolute md:invisible relative ri-menu-3-line"></i>
+        <div className=" flex gap-7 items-center justify-center   ">
+          <Link
+            to="/Home"
+            className=" hidden md:block  text-xl font-semibold text-black/80"
+          >
+            Home
+          </Link>
+          <Link
+            to="/Card"
+            className=" hidden md:block  text-xl font-semibold text-black/80"
+          >
+            Card
+          </Link>
+          <Link
+            to="/Contact"
+            className=" hidden md:block  text-xl font-semibold text-black/80"
+          >
+            Contact Us
+          </Link>
+        </div>
+        <i className=" md:hidden  ri-menu-3-line"></i>
       </div>
     </div>
   );
